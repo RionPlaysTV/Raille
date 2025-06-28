490d05c42893f8900433c965fceafcf30b6e8d55
RionPlaysTV <162782733+RionPlaysTV@users.noreply.github.com>
2025-06-28 15:05:41 +0100

    Update homepage URL and base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/raille/', // Set to '/<repo-name>/' for GitHub Pages
+  base: '/Raille/', // Set to '/<repo-name>/' for GitHub Pages
 });
